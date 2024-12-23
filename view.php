b2b893f7fc53251f9f57780445358d133cf8f3d6
Huma Riaz <riazhuma2005@gmail.com>
2024-12-23 22:17:17 +0500

    Update view.php

--- view.php
+++ view.php
@@ -1,4 +1,6 @@
-<?php include "conn.php"; ?>
+<?php include "conn.php"; 
+echo "<br><br>" ?>
+<a href="form.php">FORM:</a>
 
 <table border = '1px' cellpadding = '10px'>
     <tr>
@@ -7,4 +9,28 @@
         <th>Age</th>
         <th colspan = "2">Actions</th>
     </tr>
+
+<?php 
+$query= "SELECT * from students";
+$data= mysqli_query($con,$query);
+$result=mysqli_num_rows($data);
+
+if($result) {
+    while($row=mysqli_fetch_array($data)) {
+        ?>
+        <tr>
+        <td><?php echo $row['name']; ?> </td>
+        <td><?php echo $row['email']; ?> </td>
+        <td><?php echo $row['age']; ?> </td>
+        </tr>
+    <?php
+    }
+} else{
+    ?>
+    <tr>
+        <td>No Record</td>
+    </tr>
+    <?php
+}
+?>
 </table>
